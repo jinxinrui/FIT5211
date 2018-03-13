e082a17ab0b58fea183bbee2621b29cdd3ef5524
ushinonagisa <zhengsheng8@icloud.com>
2018-03-13 11:50:48 +1100

    update coingame using dictionary, reduce the running time

--- week3/CoinGame.py
+++ week3/CoinGame.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+import time
 
 
 def coingame(n):
@@ -29,6 +30,38 @@ def coingame(n):
         return temp
 
 
+def coingameImprove(n, dic=None):
+    temp = np.array([0, 0])
+    if dic is None:
+        if n <= 4:
+            dic = [None] * 4
+        else:
+            dic = [None] * n
+        dic[0] = np.array([1, 1])
+        dic[1] = np.array([1, 1])
+        dic[2] = np.array([0, 2])
+        dic[3] = np.array([1, 1]) + dic[2]
+    if n == 1:
+        return [dic[0], dic]
+    elif n == 2:
+        return [dic[1], dic]
+    elif n == 3:
+        return [dic[2], dic]
+    elif n == 4:
+        return [dic[3], dic]
+    elif dic[n - 1] is not None:
+        return [dic[n - 1], dic]
+    else:
+        res = np.array([1, 0]) + coingameImprove(n - 1, dic)[0]
+        temp = calres(res, temp)
+        res = np.array([1, 0]) + coingameImprove(n - 2, dic)[0]
+        temp = calres(res, temp)
+        res = np.array([1, 0]) + coingameImprove(n - 4, dic)[0]
+        temp = calres(res, temp)
+        dic[n - 1] = temp
+        return [temp, dic]
+
+
 def calres(res, temp):
     if res[0] % 2 == 1:
         if temp[0] == 1:
@@ -42,4 +75,12 @@ def calres(res, temp):
     return temp
 
 
-coingame(4)  # 1 means alice, 0 means Bob
+start1 = time.time()
+coingame(30)  # 1 means alice, 0 means Bob
+end1 = time.time()
+start2 = time.time()
+coingameImprove(30)
+end2 = time.time()
+print("time: " + str(end1 - start1))
+
+print("time: " + str(end2 - start2))
